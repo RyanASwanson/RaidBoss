c96ca3e1e7372a1e05c3e88d09b39146c357d452
rswansonbradley <raswanson@mail.bradley.edu>
2024-04-23 14:48:43 -0500

    SelectionManagerUpdates

--- Assets/Scripts/Managers/Universal/SelectionManager.cs
+++ Assets/Scripts/Managers/Universal/SelectionManager.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class SelectionManager : BaseUniversalManager
 {
+    private HeroSO[] _selectedHeroes = new HeroSO[_maxHeroes];
+    private int _selectedCount = 0;
+    private const int _maxHeroes = 5;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    public void AddNewSelectedHero(HeroSO newHeroSO)
+    {
+        if (_selectedCount < _maxHeroes)
+        {
+            _selectedHeroes[_selectedCount] = newHeroSO;
+            _selectedCount++;
+        }
+    }
+
+    public void RemoveSpecificHero(HeroSO removingHero)
+    {
+        
+    }
+
+
+    #region Getters
+    public HeroSO[] GetAllSelectedHeroes() => _selectedHeroes;
+    public HeroSO GetHeroAtValue(int val) => _selectedHeroes[val];
+    public int GetSelectedHeroesCount() => _selectedCount;
+    #endregion
 }
---
+++ Assets/Scripts/Menus/SelectionScene/SelectHeroButton.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectHeroButton : MonoBehaviour
+{
+    [SerializeField] private HeroSO _associatedHero;
+    private bool _buttonHasBeenPressed = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ButtonPressed()
+    {
+        if(!_buttonHasBeenPressed)
+        {
+            UniversalManagers.Instance.GetSelectionManager().AddNewSelectedHero(_associatedHero);
+        }
+    }
+}
