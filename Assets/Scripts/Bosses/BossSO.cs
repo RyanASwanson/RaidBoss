e27f6ef5ee4844ba728496b391067cea7819af0e
rswansonbradley <raswanson@mail.bradley.edu>
2024-04-25 11:48:31 -0500

    BossSO

---
+++ Assets/Scripts/Bosses/BossSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BossScriptableObject", menuName = "BossScriptableObject", order = 3)]
+public class BossSO : ScriptableObject
+{
+    [Header("General")]
+    [SerializeField] private string _name;
+
+    [Header("Stats")]
+    [SerializeField] private float _maxHP;
+}
--- Assets/Scripts/Heroes/HeroSO.cs
+++ Assets/Scripts/Heroes/HeroSO.cs
@@ -15,7 +15,7 @@ public class HeroSO : ScriptableObject
 
     [Header("Visuals")]
     [SerializeField] private Sprite _heroIcon;
-    [SerializeField] private MeshFilter _mesh;
+    [SerializeField] private GameObject _model;
 
     #region Getters
     public string GetHeroName() => _name;
