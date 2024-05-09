73d2af003a1dc5dff1984b9262f1d2ec3208d0a7
rswansonbradley <raswanson@mail.bradley.edu>
2024-05-09 12:48:05 -0500

    MaterialsAndRandomTargetting

--- Assets/Scripts/Bosses/BossSpecific/SpecificBossFramework.cs
+++ Assets/Scripts/Bosses/BossSpecific/SpecificBossFramework.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public abstract class SpecificBossFramework : MonoBehaviour
 {
-
     internal BossBase myBossBase;
 
 
     public virtual HeroBase DetermineAggroTarget()
     {
+        float totalAggroWeight = 0;
+        foreach(HeroBase hb in GameplayManagers.Instance.GetHeroesManager().GetCurrentLivingHeroes())
+        {
+            totalAggroWeight += hb.GetHeroStats().GetCurrentAggro();
+        }
+
+        float randomWeightValue = Random.Range(0, totalAggroWeight);
+
+        float currentWeightProgress = 0;
+        foreach (HeroBase hb in GameplayManagers.Instance.GetHeroesManager().GetCurrentLivingHeroes())
+        {
+            currentWeightProgress += hb.GetHeroStats().GetCurrentAggro();
+
+            if (randomWeightValue <= currentWeightProgress)
+                return hb;
+        }
 
         return null;
     }
