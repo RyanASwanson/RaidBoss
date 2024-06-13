9df9325e2967293942d56390e51411055f7a5bcf
Ryan S. <raswanson@mail.bradley.edu>
2024-06-13 09:37:07 -0500

    CameraShakeSetup

--- Assets/Scripts/Managers/Gameplay/CameraGameManager.cs
+++ Assets/Scripts/Managers/Gameplay/CameraGameManager.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
+/// <summary>
+/// Controls the functionality of the camera during gameplay
+/// </summary>
 public class CameraGameManager : BaseGameplayManager
 {
     [SerializeField] private Camera _gameplayCamera;
+    [SerializeField] private CinemachineVirtualCamera _virtualCamera;
+
+    private CinemachineBasicMultiChannelPerlin _multiChannelPerlin;
+
+    private Coroutine _cameraShakeCoroutine;
+
+    public void StartCameraShake(float intensity, float duration)
+    {
+        if (_cameraShakeCoroutine != null)
+            StopCoroutine(_cameraShakeCoroutine);
+
+        _cameraShakeCoroutine = StartCoroutine(CameraShake(intensity,duration));
+    }
+
+    private IEnumerator CameraShake(float intensity, float duration)
+    {
+        _multiChannelPerlin.m_AmplitudeGain += intensity;
+        yield return new WaitForSeconds(duration);
+        _multiChannelPerlin.m_AmplitudeGain -= intensity;
+    }
+
     public override void SetupGameplayManager()
     {
         base.SetupGameplayManager();
+
+        _multiChannelPerlin = _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+
+        //StartCameraShake(10, 3);
     }
 
 
