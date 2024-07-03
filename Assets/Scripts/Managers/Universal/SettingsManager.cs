8f9d5fb5268642b05129b3da6f567bdd76b10502
Ryan S. <raswanson@mail.bradley.edu>
2024-07-03 11:51:12 -0500

    TimeManagerAndUniversalSetup

--- Assets/Scripts/Managers/Universal/SettingsManager.cs
+++ Assets/Scripts/Managers/Universal/SettingsManager.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsManager : MonoBehaviour
+public class SettingsManager : BaseUniversalManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void SetupUniversalManager()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        throw new System.NotImplementedException();
     }
 }
--- Assets/Scripts/Managers/Universal/TimeManager.cs
+++ Assets/Scripts/Managers/Universal/TimeManager.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeManager : MonoBehaviour
+/// <summary>
+/// Handles the speed at which the game plays
+/// </summary>
+public class TimeManager : BaseUniversalManager
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private bool _useSlowestTime;
+
+    private List<float> _appliedTimeVariations = new List<float>();
+
+
+    public override void SetupUniversalManager()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Adds a new value to the appliedTimeVariations
+    /// </summary>
+    /// <param name="timeVariation"></param> the speed the time is being set to
+    /// <param name="duration"></param> duration is relative to the current time scale
+    public void AddNewTimeVariationForDuration(float timeVariation, float duration)
     {
-        
+
+    }
+
+    public void SetTimeToNormalSpeed()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void ReturnToPreviousSpeed()
+    {
+
+    }
+
+    public void FreezeTime()
+    {
+        Time.timeScale = 0;
     }
 }
