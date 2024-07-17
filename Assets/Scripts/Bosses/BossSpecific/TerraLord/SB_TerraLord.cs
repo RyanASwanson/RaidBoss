c8dd46852f009f325cc0c858dba449da074b31c8
Ryan S. <raswanson@mail.bradley.edu>
2024-07-17 14:08:50 -0500

    TerraLordScriptAndPrefabSetup

---
+++ Assets/Scripts/Bosses/BossSpecific/TerraLord/SB_TerraLord.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SB_TerraLord : SpecificBossFramework
+{
+    public override void SubscribeToEvents()
+    {
+        base.SubscribeToEvents();
+    }
+}
