426465ff9d84aa563dc6f6949ddc273eec4a8559
Ryan S. <raswanson@mail.bradley.edu>
2024-08-15 19:59:23 -0500

    ImprovedMeteorTargetting

--- Assets/Scripts/Bosses/BossSpecific/MagmaLord/Meteor/SBA_Meteor.cs
+++ Assets/Scripts/Bosses/BossSpecific/MagmaLord/Meteor/SBA_Meteor.cs
@@ -14,6 +14,8 @@ public class SBA_Meteor : SpecificBossAbilityFramework
     [SerializeField] private GameObject _movingMeteor;
     [SerializeField] private GameObject _targetZone;
 
+    [SerializeField] private GameObject _directionalTargetZone;
+
     //[SerializeField] private Vector3 _fallingMeteorAngleVariance;
 
     private GameObject _storedFallingMeteor;
@@ -27,6 +29,14 @@ public class SBA_Meteor : SpecificBossAbilityFramework
     protected override void StartShowTargetZone()
     {
         _currentTargetZones.Add(Instantiate(_targetZone, _storedTargetLocation, Quaternion.identity));
+
+        GameObject newDirectionalTargetZone = Instantiate(_directionalTargetZone, 
+            _storedTargetLocation, Quaternion.identity);
+
+        _currentTargetZones.Add(newDirectionalTargetZone);
+
+        StartCoroutine(FollowingDirectionalTargetZone(newDirectionalTargetZone));
+
         base.StartShowTargetZone();
     }
 
@@ -36,6 +46,52 @@ public class SBA_Meteor : SpecificBossAbilityFramework
         _storedFallingMeteor.GetComponent<SBP_FallingMeteor>().AdditionalSetup(_storedTarget.gameObject);
 
         base.StartAbilityWindUp();
+    }
+
+    protected IEnumerator FollowingDirectionalTargetZone(GameObject targetZone)
+    {
+        StartCoroutine(HideTargetZoneUntilNonZero(targetZone));
+
+        while(targetZone != null && _storedTarget != null)
+        {
+            targetZone.transform.LookAt(_storedTarget.transform.position);
+            targetZone.transform.eulerAngles = new Vector3(0, targetZone.transform.eulerAngles.y, 0);
+            yield return null;
+        }
+    }
+
+    protected IEnumerator HideTargetZoneUntilNonZero(GameObject targetZone)
+    {
+        targetZone.SetActive(false);
+
+        /* while (targetZone != null && _storedTarget != null &&
+             targetZone.transform.position.x == _storedTarget.transform.position.x &&
+             targetZone.transform.position.z == _storedTarget.transform.position.z)
+         {
+             print(targetZone.transform.position);
+             print(_storedTarget.transform.position);
+             yield return null;
+         }
+
+         print(targetZone.transform.position);
+         print(_storedTarget.transform.position);
+         print(targetZone.transform.position.x == _storedTarget.transform.position.x);
+         print(targetZone.transform.position.z == _storedTarget.transform.position.z);
+         Debug.Log("Active");*/
+
+        //If that direction is close to zero choose a random direction instead
+        while (Mathf.Abs(targetZone.transform.position.x - _storedTarget.transform.position.x) < .1f &&
+            Mathf.Abs(targetZone.transform.position.z - _storedTarget.transform.position.z) < .1f)
+            yield return null;
+
+        print(targetZone.transform.position);
+        print(_storedTarget.transform.position);
+
+        print(Mathf.Abs(targetZone.transform.position.x - _storedTarget.transform.position.x));
+        print(Mathf.Abs(targetZone.transform.position.z - _storedTarget.transform.position.z));
+
+        if (targetZone != null)
+            targetZone.SetActive(true);
     }
 
     protected void FallingMeteorContact()
@@ -54,4 +110,6 @@ public class SBA_Meteor : SpecificBossAbilityFramework
         _storedMovingMeteor.GetComponent<SBP_FollowingMeteor>().AdditionalSetup(_storedTarget);
         base.AbilityStart();
     }
+
+    
 }
