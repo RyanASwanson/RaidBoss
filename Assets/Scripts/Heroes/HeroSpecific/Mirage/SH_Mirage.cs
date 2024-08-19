bd248ca5493f291f128ab235479416cfc13d4f0c
Ryan S. <raswanson@mail.bradley.edu>
2024-08-19 10:22:49 -0500

    MirageCloneWork

--- Assets/Scripts/Heroes/HeroSpecific/Mirage/SH_Mirage.cs
+++ Assets/Scripts/Heroes/HeroSpecific/Mirage/SH_Mirage.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class SH_Mirage : SpecificHeroFramework
 {
-    // Start is called before the first frame update
-    void Start()
+    [Space]
+    [SerializeField] private GameObject _basicProjectile;
+
+    [SerializeField] private GameObject _manualClone;
+    private const float _cloneSpawnOffset = -2;
+
+    private HeroBase _cloneBase;
+    private MirageClone _associatedCloneFunc;
+
+
+    #region Basic Abilities
+
+    #endregion
+
+    #region Manual Abilities
+    private void CreateClone()
     {
-        
+        Vector3 spawnLocation = _myHeroBase.transform.position + (_myHeroBase.transform.forward * _cloneSpawnOffset);
+        GameObject cloneObject = Instantiate(_manualClone, spawnLocation, _myHeroBase.transform.rotation);
+
+        _cloneBase = cloneObject.GetComponent<HeroBase>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void ActivateManualAbilities(Vector3 attackLocation)
     {
-        
+        base.ActivateManualAbilities(attackLocation);
+        MoveClone(attackLocation);
     }
+
+    private void MoveClone(Vector3 moveLocation)
+    {
+        _cloneBase.GetPathfinding().DirectNavigationTo(moveLocation);
+    }
+    #endregion
+
+    #region Passive Abilities
+
+    #endregion
+
+    public override void SetupSpecificHero(HeroBase heroBase, HeroSO heroSO)
+    {
+        base.SetupSpecificHero(heroBase, heroSO);
+        CreateClone();
+    }
+
+
 }
