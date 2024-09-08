e50cc368e67661038a98a5b1321e3dbbfd66b3a1
Ryan S. <raswanson@mail.bradley.edu>
2024-09-08 13:37:45 -0500

    OrganizingFolders

--- Assets/Scripts/Bosses/BossSpecific/GlacialLord/SB_GlacialLord.cs
+++ Assets/Scripts/Bosses/BossSpecific/GlacialLord/SB_GlacialLord.cs
@@ -4,4 +4,21 @@ using UnityEngine;
 
 public class SB_GlacialLord : SpecificBossFramework
 {
+    [SerializeField] private Vector3 _iceCrownSpawnLocation;
+    [SerializeField] private GameObject _iceCrown;
+
+    private GlacialLordIceCrown _currentIceCrown;
+
+    protected override void StartFight()
+    {
+        base.StartFight();
+        CreateIceCrown();
+    }
+
+    private void CreateIceCrown()
+    {
+        GameObject crown = Instantiate(_iceCrown, _iceCrownSpawnLocation, Quaternion.identity);
+
+        _currentIceCrown = crown.GetComponent<GlacialLordIceCrown>();
+    }
 }
