377866eba9577b2f6f6e3fc8d101894a182d615f
Ryan S. <raswanson@mail.bradley.edu>
2024-09-12 17:20:49 -0500

    MoreIcicleRainSetup

---
+++ Assets/Scripts/Bosses/BossSpecific/GlacialLord/Abilities/IcicleRain/SBP_IcicleRain.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SBP_IcicleRain : BossProjectileFramework
+{
+    [SerializeField] private float _iciclesSeperationTime;
+    [SerializeField] private int _iciclesPerSet;
+
+    [SerializeField] private List<GameObject> _icicles;
+
+    private IEnumerator SpikeSpawningProcess()
+    {
+        int setCounter = 1;
+        for (int i = 0; i <= _icicles.Count - 1; i++)
+        {
+            SpawnIcicle(_icicles[i]);
+
+            setCounter++;
+
+            if(setCounter >= _iciclesPerSet)
+            {
+                setCounter = 1;
+                yield return new WaitForSeconds(_iciclesSeperationTime);
+            }
+            
+        }
+    }
+
+
+    private void SpawnIcicle(GameObject spike)
+    {
+        spike.SetActive(true);
+    }
+
+
+    #region Base Ability
+    public override void SetUpProjectile(BossBase bossBase)
+    {
+        base.SetUpProjectile(bossBase);
+        StartCoroutine(SpikeSpawningProcess());
+    }
+    #endregion
+}
