dbe1feeb396881c423e2ddea3b3556a3f831f777
Ryan S. <raswanson@mail.bradley.edu>
2024-09-12 19:07:59 -0500

    MoreIcicleWork

--- Assets/Scripts/Bosses/BossSpecific/GlacialLord/Abilities/IcicleRain/SBP_Icicle.cs
+++ Assets/Scripts/Bosses/BossSpecific/GlacialLord/Abilities/IcicleRain/SBP_Icicle.cs
@@ -6,8 +6,17 @@ public class SBP_Icicle : BossProjectileFramework
 {
     [SerializeField] private GlacialLordSelfMinionHit _minionHit;
 
+    public void IcicleContactCheck()
+    {
+        _minionHit.MinionContactFromDistance();
+
+        Destroy(gameObject);
+    }
+
     public override void SetUpProjectile(BossBase bossBase)
     {
         base.SetUpProjectile(bossBase);
     }
+
+
 }
