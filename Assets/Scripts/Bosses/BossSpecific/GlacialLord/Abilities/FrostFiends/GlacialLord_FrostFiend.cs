c31b702675767c76e37593d8fa6b2322966fc261
Ryan S. <raswanson@mail.bradley.edu>
2024-10-12 15:29:59 -0500

    FrostFiendFreezeBasicAnim

--- Assets/Scripts/Bosses/BossSpecific/GlacialLord/Abilities/FrostFiends/GlacialLord_FrostFiend.cs
+++ Assets/Scripts/Bosses/BossSpecific/GlacialLord/Abilities/FrostFiends/GlacialLord_FrostFiend.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GlacialLord_FrostFiend : BossMinionBase
 {
+    [SerializeField] private Animator _frostFiendAnimator;
+
+    private const string _fiendFrozenAnimTrigger = "FiendFrozen";
+    private const string _fiendUnfrozenAnimTrigger = "FiendUnfrozen";
+
     private bool _minionFrozen;
     private float _freezeDuration;
 
@@ -12,11 +17,14 @@ public class GlacialLord_FrostFiend : BossMinionBase
         _freezeDuration = freezeDuration;
     }
 
+    #region Freezing
     public void FreezeMinion()
     {
         if (_minionFrozen) return;
 
         _minionFrozen = true;
+        FreezeAnimationTrigger();
+
         StartCoroutine(FreezeProcess());
     }
 
@@ -29,7 +37,20 @@ public class GlacialLord_FrostFiend : BossMinionBase
     private void UnfreezeMinion()
     {
         _minionFrozen = false;
+        UnfreezeAnimationTrigger();
     }
+
+    private void FreezeAnimationTrigger()
+    {
+        _frostFiendAnimator.SetTrigger(_fiendFrozenAnimTrigger);
+    }
+
+    private void UnfreezeAnimationTrigger()
+    {
+        _frostFiendAnimator.SetTrigger(_fiendUnfrozenAnimTrigger);
+    }
+
+    #endregion
 
     #region Getters
     public bool IsMinionFrozen() => _minionFrozen;
