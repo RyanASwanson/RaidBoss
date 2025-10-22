bdb28832897c448f1f88f4ca465650c286c2cb8a
Ryan Swanson <raswanson@mail.bradley.edu>
2025-10-22 14:21:52 -0500

    AstromancerAnimationsAndSfx

--- Assets/Scripts/Heroes/HeroSpecific/Astromancer/SH_Astromancer.cs
+++ Assets/Scripts/Heroes/HeroSpecific/Astromancer/SH_Astromancer.cs
@@ -31,6 +31,14 @@ public class SH_Astromancer : SpecificHeroFramework
     [SerializeField] private float _passiveRechargeManualAmount;
 
     SHP_AstromancerManualProjectile _storedManual;
+    
+    private const string MANUAL_LEFT_ANIM_TRIGGER = "ManualLeft";
+    private const string MANUAL_RIGHT_ANIM_TRIGGER = "ManualRight";
+    
+    
+    public const int BASIC_PROJECTILE_REFLECT_AUDIO_ID = 0;
+    public const int MANUAL_LEFT_AUDIO_ID = 1;
+    public const int MANUAL_RIGHT_AUDIO_ID = 2;
 
     #region Basic Abilities
     public override void ActivateBasicAbilities()
@@ -55,7 +63,7 @@ public class SH_Astromancer : SpecificHeroFramework
         //Performs the setup for the 'healing' area so that it knows it's owner
         spawnedProjectile.GetComponent<GeneralHeroHealArea>().SetUpHealingArea(_myHeroBase);
     }
-
+    
     #endregion
 
     #region Manual Abilities
@@ -129,10 +137,15 @@ public class SH_Astromancer : SpecificHeroFramework
         if (_isManualRight)
         {
             spawnedProjectile = Instantiate(_purpleManualProjectile, transform.position, Quaternion.identity);
+            PlayRightManualAudio();
+            ManualRightAnimation();
+            
         }
         else
         {
             spawnedProjectile = Instantiate(_blueManualProjectile, transform.position, Quaternion.identity);
+            PlayLeftManualAudio();
+            ManualLeftAnimation();
         }
         
         SHP_AstromancerManualProjectile astromancerManual = spawnedProjectile.GetComponent<SHP_AstromancerManualProjectile>();
@@ -141,6 +154,30 @@ public class SH_Astromancer : SpecificHeroFramework
         astromancerManual.SetUpProjectile(_myHeroBase, EHeroAbilityType.Manual);
         
         _isManualRight = !_isManualRight;
+    }
+    
+    private void ManualLeftAnimation()
+    {
+        _myHeroBase.GetHeroVisuals().HeroSpecificAnimationTrigger(MANUAL_LEFT_ANIM_TRIGGER);
+    }
+    
+    private void ManualRightAnimation()
+    {
+        _myHeroBase.GetHeroVisuals().HeroSpecificAnimationTrigger(MANUAL_RIGHT_ANIM_TRIGGER);
+    }
+
+    private void PlayLeftManualAudio()
+    {
+        AudioManager.Instance.PlaySpecificAudio(
+            AudioManager.Instance.AllSpecificHeroAudio[_myHeroBase.GetHeroSO().GetHeroID()]
+                .MiscellaneousHeroAudio[MANUAL_LEFT_AUDIO_ID]);
+    }
+
+    private void PlayRightManualAudio()
+    {
+        AudioManager.Instance.PlaySpecificAudio(
+            AudioManager.Instance.AllSpecificHeroAudio[_myHeroBase.GetHeroSO().GetHeroID()]
+                .MiscellaneousHeroAudio[MANUAL_RIGHT_AUDIO_ID]);
     }
 
     private void StopManualProcess()
@@ -171,7 +208,9 @@ public class SH_Astromancer : SpecificHeroFramework
     public override void ActivatePassiveAbilities()
     {
         if (!_manualActive)
+        {
             AddToManualAbilityChargeTime(_passiveRechargeManualAmount);
+        }
     }
 
     #endregion
