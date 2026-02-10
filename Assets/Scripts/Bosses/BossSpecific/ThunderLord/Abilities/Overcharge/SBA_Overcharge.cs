4a5d71ed170e3953768220b9a7ca5c95db6594f5
Ryan Swanson <raswanson@mail.bradley.edu>
2026-02-09 21:22:08 -0600

    ThunderLordAudioAdjustments

--- Assets/Scripts/Bosses/BossSpecific/ThunderLord/Abilities/Overcharge/SBA_Overcharge.cs
+++ Assets/Scripts/Bosses/BossSpecific/ThunderLord/Abilities/Overcharge/SBA_Overcharge.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class SBA_Overcharge : SpecificBossAbilityFramework
 {
-    [SerializeField] private GameObject _overChargeTargetZone;
-
-    protected override void AbilityPrep()
-    {
-        base.AbilityPrep();
-    }
+    [SerializeField] private GameObject _overchargeTargetZone;
+    [SerializeField] private GameObject _overchargeTargetVFX;
+    
     
     protected override void StartShowTargetZone()
     {
         base.StartShowTargetZone();
-        BossTargetZoneParent targetZone = Instantiate(_overChargeTargetZone, _storedTargetLocation, Quaternion.identity).GetComponent<BossTargetZoneParent>();
+        BossTargetZoneParent targetZone = Instantiate(_overchargeTargetZone, _storedTargetLocation, Quaternion.identity).GetComponent<BossTargetZoneParent>();
         _currentTargetZones.Add(targetZone);
         SB_ThunderLord.Instance.ChildGameObjectToImpendingStorm(targetZone.gameObject);
+        
+        GameObject overchargeTargetVFX = Instantiate(_overchargeTargetVFX, _storedTargetLocation,Quaternion.identity);
+        SB_ThunderLord.Instance.ChildGameObjectToImpendingStorm(overchargeTargetVFX);
         
         BossVisuals.Instance.BossLookAt(targetZone.GetBossTargetZones()[0].GetAdditionalGameObjectReferences()[0], _abilityWindUpTime);
     }
