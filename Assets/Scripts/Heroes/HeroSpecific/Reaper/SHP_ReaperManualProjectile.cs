39653d0f1bd383e0d7276616303d31d0d29f5d0f
Ryan Swanson <raswanson@mail.bradley.edu>
2026-05-19 16:11:44 -0500

    ReaperAndShamanManualFixes

--- Assets/Scripts/Heroes/HeroSpecific/Reaper/SHP_ReaperManualProjectile.cs
+++ Assets/Scripts/Heroes/HeroSpecific/Reaper/SHP_ReaperManualProjectile.cs
@@ -12,6 +12,15 @@ using UnityEngine;
 public class SHP_ReaperManualProjectile : HeroProjectileFramework
 {
     [SerializeField] private float _projectileSpeed;
+
+    [SerializeField] private float _projectileHitboxMaxShrinkRange;
+    [SerializeField] private float _projectileHitboxMinimumSize;
+    private float _projectileHitboxMaximumSize;
+    
+    [Space]
+    [SerializeField] private CurveProgression _removalCurve;
+    [SerializeField] private GeneralVFXFunctionality _projectileVFX;
+    [SerializeField] private SphereCollider _projectileCollider;
     
     private Animator _projectileAnimator;
 
@@ -20,9 +29,15 @@ public class SHP_ReaperManualProjectile : HeroProjectileFramework
     private IEnumerator MoveProjectile()
     {
         Vector3 targetLocation = _myHeroBase.gameObject.transform.position;
+        float distanceToTarget;
+        
         while (true)
         {
-            if (!_myHeroBase.IsUnityNull())
+            if (_myHeroBase.IsUnityNull())
+            {
+                HeroNotFound();
+            }
+            else
             {
                 targetLocation = _myHeroBase.gameObject.transform.position;
             }
@@ -30,15 +45,35 @@ public class SHP_ReaperManualProjectile : HeroProjectileFramework
             transform.position = Vector3.MoveTowards(transform.position,
                 targetLocation, _projectileSpeed * Time.deltaTime);
 
-            transform.LookAt(_myHeroBase.gameObject.transform.position);
+            transform.LookAt(targetLocation);
             transform.eulerAngles.Set(0, transform.eulerAngles.y, 0);
+            
+            distanceToTarget = Vector3.Distance(transform.position, targetLocation);
+            if (distanceToTarget < _projectileHitboxMaxShrinkRange)
+            {
+                _projectileCollider.radius = Mathf.Lerp(distanceToTarget/_projectileHitboxMaxShrinkRange, _projectileHitboxMinimumSize, _projectileHitboxMaximumSize);
+            }
+            
             yield return null;
         }
     }
-
+    
     public void PlayProjectileHitFlickerAnimation()
     {
         _projectileAnimator.SetTrigger(HIT_ANIM_TRIGGER);
+    }
+
+    private void HeroNotFound()
+    {
+        _removalCurve.StartMovingDownOnCurve();
+    }
+
+    public void ProjectileRemovedEarly()
+    {
+        _projectileVFX.StopAllParticleSystems();
+        _projectileVFX.DetachVisualEffect();
+        _projectileVFX.StartDelayedLifetime();
+        Destroy(gameObject);
     }
 
     #region Base Ability
@@ -50,6 +85,8 @@ public class SHP_ReaperManualProjectile : HeroProjectileFramework
     {
         base.SetUpProjectile(heroBase, heroAbilityType);
         _projectileAnimator = GetComponentInChildren<Animator>();
+        _projectileHitboxMaximumSize = _projectileCollider.radius;
+        
         StartCoroutine(MoveProjectile());
     }
     #endregion
